d25cc6f8aee932e9aa6f8aa533da41a298b69d0a
Leandro da Silva de Souza <leandro.souza.159@gmail.com>
2023-07-12 14:24:38 -0300

    Implementando polimorfismo

--- modelo.py
+++ modelo.py
@@ -19,22 +19,43 @@ class Programa:
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
 
+    def __str__(self) -> str:
+        return f'{self._nome} - {self.ano} - {self._likes}'
+    
+    def __repr__(self) -> str:
+        return f'Programa(\'{self._nome}\', {self.ano})'
+
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
+
+    def __str__(self) -> str:
+        return f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes}'
+
+    def __repr__(self) -> str:
+        return f'Filme(\'{self._nome}\', {self.ano}, {self.duracao})'        
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def __str__(self) -> str:
+        return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes}' 
+
+    def __repr__(self) -> str:
+        return f'Serie(\'{self._nome}\', {self.ano}, {self.temporadas})'           
+
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 vingadores.dar_like()
-print(f'{vingadores.nome} - {vingadores.duracao}: {vingadores.likes}')
-
 
 atlanta = Serie('atlanta', 2018, 2)
 atlanta.dar_like()
 atlanta.dar_like()
-print(f'{atlanta.nome} - {atlanta.temporadas}: {atlanta.likes}')
+
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    print(programa)
+    print(repr(programa))
