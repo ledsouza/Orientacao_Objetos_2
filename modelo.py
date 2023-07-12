7ab7686e53f2fda9e0918be37e606c211a2e71b9
Leandro da Silva de Souza <leandro.souza.159@gmail.com>
2023-07-12 15:32:28 -0300

    Criando a classe Playlist e implementando Duck typing

--- modelo.py
+++ modelo.py
@@ -45,17 +45,47 @@ class Serie(Programa):
         return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes}' 
 
     def __repr__(self) -> str:
-        return f'Serie(\'{self._nome}\', {self.ano}, {self.temporadas})'           
+        return f'Serie(\'{self._nome}\', {self.ano}, {self.temporadas})'
+
+class Playlist():
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
+    
+    def __getitem__(self, item):
+        return self._programas[item]
+    
+    def __len__(self):
+        return len(self._programas)
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-vingadores.dar_like()
-
 atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('Todo mundo em pânico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
+
+vingadores.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+atlanta.dar_like()
 atlanta.dar_like()
 atlanta.dar_like()
 
-filmes_e_series = [vingadores, atlanta]
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+minha_playlist  = Playlist('fim de semana', filmes_e_series)
 
-for programa in filmes_e_series:
+print(f'Tamanho do playlist: {len(minha_playlist)}')
+
+for programa in minha_playlist:
     print(programa)
-    print(repr(programa))
